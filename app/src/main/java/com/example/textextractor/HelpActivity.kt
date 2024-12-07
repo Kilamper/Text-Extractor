54c5986d2b8b77d5ad06f66d5c59ddca44d6b875
Pablo <zerytv75@gmail.com>
2024-12-07 12:15:29 +0000

    Help activity con comprobacion de login

--- app/src/main/java/com/example/textextractor/HelpActivity.kt
+++ app/src/main/java/com/example/textextractor/HelpActivity.kt
@@ -3,16 +3,21 @@ package com.example.textextractor
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.MenuInflater
 import android.widget.Button
 import android.widget.PopupMenu
 import android.widget.Toast
-import androidx.core.content.ContextCompat.startActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class HelpActivity : AppCompatActivity() {
 
     private lateinit var menuBtn: Button
     private lateinit var logInBtn: Button
+    private lateinit var userIcon: Button
+    private var currentUser: FirebaseUser? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +25,7 @@ class HelpActivity : AppCompatActivity() {
 
         menuBtn = findViewById(R.id.menuBtn)
         logInBtn = findViewById(R.id.logInBtn)
+        userIcon = findViewById(R.id.userIcon)
 
         // Menu button
         menuBtn.setOnClickListener {
@@ -52,6 +58,42 @@ class HelpActivity : AppCompatActivity() {
                 }
             }
             popupMenu.show()
+        }
+
+        currentUser = FirebaseAuth.getInstance().currentUser
+
+        if (currentUser != null) {
+            userIcon.visibility = Button.VISIBLE
+            logInBtn.visibility = Button.INVISIBLE
+            val widthInDp = 50
+            val widthInPx = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                widthInDp.toFloat(),
+                resources.displayMetrics
+            ).toInt()
+            userIcon.layoutParams.width = widthInPx
+            logInBtn.layoutParams.width = 0
+            userIcon.setOnClickListener {
+                val popupMenu = PopupMenu(this, userIcon)
+                val inflater: MenuInflater = menuInflater
+                inflater.inflate(R.menu.user_options, popupMenu.menu)
+
+                // Set the user email in the menu
+                val userEmailMenuItem = popupMenu.menu.findItem(R.id.userEmail)
+                userEmailMenuItem.title = currentUser?.email ?: ""
+
+                popupMenu.setOnMenuItemClickListener { item ->
+                    when (item.itemId) {
+                        R.id.logOut -> {
+                            FirebaseAuth.getInstance().signOut()
+                            recreate()
+                            true
+                        }
+                        else -> false
+                    }
+                }
+                popupMenu.show() // Show the popup menu
+            }
         }
 
         // Log in button
