282ba7a622786d1a54fccca9a6a42241f655df47
Pablo <zerytv75@gmail.com>
2024-12-07 10:10:13 +0000

    Help activity e hist activity

--- app/src/main/java/com/example/textextractor/HelpActivity.kt
+++ app/src/main/java/com/example/textextractor/HelpActivity.kt
@@ -1,13 +1,27 @@
 package com.example.textextractor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HelpActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?){
-        setTheme(R.style.Theme_TextExtractor)
 
+    private lateinit var goBack: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_help)
+
+        goBack = findViewById(R.id.goBack)
+
+        goBack.setOnClickListener {
+            goToMain()
+        }
+    }
+
+    private fun goToMain() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
